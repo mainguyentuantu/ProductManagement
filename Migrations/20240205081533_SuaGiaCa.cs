a8182a49acadd92b885219a17dec7ff7bf3e8ef8
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-02-05 15:25:33 +0700

    Them Validate cho cac thuoc tinh cua class Products

---
+++ Migrations/20240205081533_SuaGiaCa.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class SuaGiaCa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ThuongHieu",
+                table: "Products",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TenSP",
+                table: "Products",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MauSac",
+                table: "Products",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KieuDang",
+                table: "Products",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KichThuoc",
+                table: "Products",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "GiaCa",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ChatLieu",
+                table: "Products",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ThuongHieu",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TenSP",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MauSac",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KieuDang",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KichThuoc",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GiaCa",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ChatLieu",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+        }
+    }
+}
--- Models/Products.cs
+++ Models/Products.cs
@@ -5,20 +5,47 @@ namespace ProductManagement.Models
     public class Products
     {
         [Key]
-        public int MaSP { get; set; } 
-        public string? TenSP {  get; set; }
+        public int MaSP { get; set; }
+
+        [Required(ErrorMessage = "Tên sản phẩm là trường bắt buộc.")]
+        [StringLength(20, ErrorMessage = "Tên sản phẩm không được vượt quá 20 ký tự.")]
+        [RegularExpression(@"^[a-zA-Z0-9\sàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđĐ]*$",
+    ErrorMessage = "Tên sản phẩm chỉ được nhập số, chữ và ký tự tiếng Việt.")]
+        public string? TenSP { get; set; }
+
+        [StringLength(20, ErrorMessage = "Kích thước không được vượt quá 20 ký tự.")]
+        [RegularExpression(@"^[0-9*]+$", ErrorMessage = "Kích thước chỉ được nhập số và dấu (*).")]
         public string? KichThuoc { get; set; }
+
+        [StringLength(15, ErrorMessage = "Chất liệu không được vượt quá 15 ký tự.")]
+        [RegularExpression(@"^[a-zA-Z\sàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđĐ]*$",
+    ErrorMessage = "Chất liệu chỉ được chữ và ký tự tiếng Việt.")]
         public string? ChatLieu { get; set; }
-        public string? MauSac {  get; set; }
+
+        [StringLength(25, ErrorMessage = "Màu sắc không được vượt quá 25 ký tự.")]
+        [RegularExpression(@"^[a-zA-Z0-9\sàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđĐ]*$",
+    ErrorMessage = "Màu sắc chỉ được số, chữ và ký tự tiếng Việt.")]
+        public string? MauSac { get; set; }
+
+        [StringLength(30, ErrorMessage = "Chất liệu không được vượt quá 30 ký tự.")]
+        [RegularExpression(@"^[a-zA-Z0-9\sàáạảãâầấậẩẫăằắặẳẵèéẹẻẽêềếệểễìíịỉĩòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữỳýỵỷỹđĐ]*$",
+    ErrorMessage = "Kiểu dáng chỉ được số, chữ và ký tự tiếng Việt.")]
         public string? KieuDang { get; set; }
-        public string? ThuongHieu { get; set; } 
-        public int GiaCa { get; set; }
-        
-      
+
+        [StringLength(30, ErrorMessage = "Chất liệu không được vượt quá 30 ký tự.")]
+        [RegularExpression(@"^[a-zA-Z0-9]*$",
+    ErrorMessage = "Chất liệu chỉ được số và chữ và không hỗ trợ kí tự tiếng Việt.")]
+        public string? ThuongHieu { get; set; }
+
+        [Required(ErrorMessage = "Giá sản phẩm là trường bắt buộc.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Giá sản phẩm không được nhập số âm.")]
+        public double GiaCa { get; set; }
+
+
     }
 
     public class ProductModel
     {
-        public List<Products> ProductList { get; set; }
+        public List<Products>? ProductList { get; set; }
     }
 }
