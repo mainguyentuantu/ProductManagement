90c6f4336421ee4455136f43036d0f8991902021
Mai Nguyễn Tuấn Tú <107095647+mainguyentuantu@users.noreply.github.com>
2024-03-04 13:43:59 +0700

    đang hoàn thành chức năng hiện giá sản phẩm trên form Order

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -27,36 +27,34 @@ namespace ProductManagement.Controllers
         // GET: Order/Create
         public IActionResult Create()
         {
-            ViewData["Products"] = _context.Products.ToList();
+            ViewData["Products"] =_context.Products.ToList();
             return View();
         }
 
-        // GET: Order/CreateConfirmed
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateConfirmed(Order order)
+        public IActionResult ProductIndex()
         {
-            var product = await _context.Products.FindAsync(order.MaSP);
-            if (product == null)
-            {
-                ModelState.AddModelError("MaSP", "Sản phẩm không tồn tại");
-                return View(order);// Hiển thị lại view với thông báo lỗi
+            //get all the initial data from the repository
+            var productlist = _context.Products;
 
-            }
-            else
-            {
-                
-                // Cập nhật giá trị dựa trên sản phẩm đã chọn
-                order.ThanhTien = order.SoLuongDH * product.GiaCa;
-
-                _context.Add(order);
-                await _context.SaveChangesAsync();
-
-                return RedirectToAction(nameof(Index));// Chuyển hướng đến view danh sách đơn hàng
-            }
-
+            //set select items for the DropDownList
+            ViewBag.Types = _context.Products.Select(c => c.TenSP).Distinct().ToList().Select(c => new SelectListItem { Text = c, Value = c }).ToList();
+            return View(productlist);
         }
+        //based on the type filter data and return partial view.
+        public IActionResult ShowProduct(string type)
+        {
+            var productlist = _context.Products.ToList();
 
+            //based on the type to filter data.
+            if (!string.IsNullOrEmpty(type) && type != "all")
+            {
+                productlist = productlist.Where(c => c.TenSP == type).ToList();
+            }
+
+            //required using Microsoft.AspNetCore.Mvc.Rendering;
+            ViewBag.Types = _context.Products.Select(c => new SelectListItem { Text = c.TenSP, Value = c.TenSP }).Distinct().ToList();
+            return PartialView("_ShowProductPartialView", productlist);
+        }
         // GET: Products/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -65,30 +63,42 @@ namespace ProductManagement.Controllers
                 return NotFound();
             }
 
-            var products = await _context.Orders
+            var order = await _context.Orders
                 .FirstOrDefaultAsync(m => m.MaDH == id);
-            if (products == null)
+            if (order == null)
             {
                 return NotFound();
             }
 
-            return View(products);
+            return View(order);
         }
         // POST: Order/Delete/1
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var products = await _context.Orders.FindAsync(id);
-            if (products != null)
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
             {
-                _context.Orders.Remove(products);
+                _context.Orders.Remove(order);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
 
         }
+        [HttpGet]
+        public async Task<IActionResult> GetGiaCa(int maSP)
+        {
+            var product = await _context.Products.FindAsync(maSP);
+            if (product != null)
+            {
+                return Json(product);
+            }
+
+            return NotFound();
+        }
+
         private bool OrderExists(int id)
         {
             return _context.Orders.Any(e => e.MaDH == id);
